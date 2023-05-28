28055183fedc84bc86ee50f7fd0a6ac669a37c74
syzygial <67280346+syzygial@users.noreply.github.com>
2023-05-28 16:15:48 -0400

    initial branch commit

--- src/core/device.c
+++ src/core/device.c
@@ -17,10 +17,7 @@ int get_device_io_by_name(hm_backend_connection_t *backend, hm_device_io_t **io,
 }
 
 int default_device_io_connect(hm_device_io_connection_t **io, hm_backend_connection_t *backend, hm_io_type_t dir) {
-  //hm_device_t *default_dev = (backend->devices)[backend->default_dev_id];
-  //hm_device_io_t *default_io = (default_dev->io_devices)[default_dev->default_io_id];
   const struct backend_info *info = get_backend_by_name(backend->backend_name);
   unsigned int io_id = (dir == PLAYBACK) ? backend->default_dev_io[0] : backend->default_dev_io[1];
-  (info->io_connect_fn)(io, backend, io_id);
-  return 0;
+  return (info->io_connect_fn)(io, backend, io_id);
 }
