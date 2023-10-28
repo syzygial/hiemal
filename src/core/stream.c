1de4ffe73779e8ab726b866c0baadf0a076faaa0
syzygial <67280346+syzygial@users.noreply.github.com>
2023-10-28 18:57:20 -0400

    .

--- src/core/stream.c
+++ src/core/stream.c
@@ -1,3 +1,20 @@
-typedef struct _stream {
+#include "intern/common.h"
 
-} hm_stream_t;
+typedef struct _encoder_stack encoder_stack;
+typedef struct _decoder_stack decoder_stack;
+
+typedef int (stream_fn)(void *src, void *dest, unsigned int n_bytes);
+
+struct _encoder {
+  HM_LIST_NODE_HEAD(struct _encoder)
+  stream_fn *encoder_fn;
+};
+
+struct _encoder_stack {
+  HM_LIST_HEAD(struct _encoder)
+};
+
+struct _stream {
+  encoder_stack *enc;
+  decoder_stack *dec;
+};
