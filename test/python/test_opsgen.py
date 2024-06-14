a4d66a041efb16b5671457df932edb6ac4993d5a
syzygial <67280346+syzygial@users.noreply.github.com>
2024-06-13 20:42:06 -0400

    Fix failing github test

--- test/python/test_opsgen.py
+++ test/python/test_opsgen.py
@@ -11,12 +11,12 @@ class TestOpGen(unittest.TestCase):
   def test_opsgen(self):
     test_str1 = """
 // [ a   : int,  b:  char]
-DSP_OP (  test1){
+DSP_OP (  test1, double, double){
   return 0;
 }
 
 // [a: char, b: int]
-DSP_OP(test2) {
+DSP_OP(test2, double*, int*) {
   return 0;
 }
 """
@@ -26,6 +26,8 @@ DSP_OP(test2) {
     self.assertTrue(str1_ops[1][0] == "test2")
     self.assertTrue(str1_ops[0][1] == ['int a', 'char b'])
     self.assertTrue(str1_ops[1][1] == ['char a', 'int b'])
+    self.assertTrue(str1_ops[0][2] == ['double src', 'double dest', 'unsigned int n_bytes'])
+    self.assertTrue(str1_ops[1][2] == ['double* src', 'int* dest', 'unsigned int n_bytes'])
   
 if __name__ == "__main__":
   unittest.main()
