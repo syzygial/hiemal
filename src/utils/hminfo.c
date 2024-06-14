1575d1201077429624d81e5c7e06051d52482add
syzygial <67280346+syzygial@users.noreply.github.com>
2024-06-13 20:50:11 -0400

    Add list of ops to hminfo output
    Signed-off-by: syzygial <67280346+syzygial@users.noreply.github.com>

--- src/utils/hminfo.c
+++ src/utils/hminfo.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
 
 #include "intern/info.h"
+#include "util/str.h"
+
+
+#include "ops.h"
 
 int main() {
-  printf("hiemal version: %s\n", hm_version_str());
+  printf("hiemal version: %s\n\n", hm_version_str());
+
+  const char **ops_list = hm_op_list();
+  printf("ops:\n");
+  while (*ops_list != NULL) {
+    printf("  %s\n", *ops_list);
+    ops_list++;
+  }
   return 0;
 }
